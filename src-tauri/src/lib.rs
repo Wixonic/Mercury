8246285b2620fbb305e9a3ae57afecd2084e8a0d
Wixonic <contact@wixonic.fr>
2026-06-20 15:37:09 +0200

    fix: Minor modifications

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,6 +2,8 @@
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_websocket::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
