bc2292574e6c67c1d080c19b0de381050ad06b6c
Wixonic <contact@wixonic.fr>
2026-06-28 01:34:16 +0200

    feat: Improve sidebar design

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -23,45 +23,72 @@ const ready = async () => {
 
 	changeState("Fetching guilds", "warning");
 
-	const createGuildElement = async (guildId: string) => {
-		const guildElement = document.createElement("button");
-		guildElement.classList.add("guild");
+	{
+		const sidebar = document.querySelector("nav.sidebar")!;
+		sidebar.classList.add("loading");
 
-		let guild: Guild<true> | Guild<false> | null = null;
-		try {
-			guild = await discordClient.guilds.get(guildId) ?? null;
-			console.log(guild);
-		} catch (error) {
-			console.error(error);
+		const createElement = async (guildId: string) => {
+			const guildElement = document.createElement("button");
+			guildElement.classList.add("guild");
+
+			let guild: Guild<true> | Guild<false> | null = null;
+			try {
+				guild = await discordClient.guilds.get(guildId) ?? null;
+				console.log(guild);
+			} catch (error) {
+				console.error(error);
+			}
+
+			if (guild) {
+				const icon = guild.icon ? document.createElement("img") : document.createElement("span");
+				icon.classList.add("icon");
+				if (guild.icon) (icon as HTMLImageElement).src = guild.icon.getURL(undefined, 256, "high", undefined, undefined, true);
+				else (icon as HTMLSpanElement).textContent = guild.name.substring(0, 2).toUpperCase();
+				guildElement.append(icon);
+			} else guildElement.innerHTML = await getIcon("question-circle");
+
+			const label = document.createElement("label");
+			label.textContent = guild ? guild.name : "Guild is unreachable";
+			guildElement.append(label);
+
+			return guildElement;
+		};
+
+		const directMessagesElement = document.querySelector("button#direct-messages")!;
+		directMessagesElement.innerHTML = await getIcon("chats-circle");
+		const directMessagesLabel = document.createElement("label");
+		directMessagesLabel.textContent = "Direct Messages";
+		directMessagesElement.append(directMessagesLabel);
+
+		const favoritesElement = document.querySelector("button#favorites")!;
+		favoritesElement.innerHTML = await getIcon("star");
+		const favoritesLabel = document.createElement("label");
+		favoritesLabel.textContent = "Favorites";
+		favoritesElement.append(favoritesLabel);
+
+		const guildContainer = document.querySelector("section.guilds")!;
+		guildContainer.classList.add("loading");
+		guildContainer.innerHTML = "";
+		for (const folder of discordClient.settings!.guild_folders) {
+			if (folder.id) {
+				for (const guildId of folder.guild_ids) guildContainer.append(await createElement(guildId));
+			} else guildContainer.append(await createElement(folder.guild_ids[0]));
 		}
 
-		if (guild) {
-			const icon = guild.icon ? document.createElement("img") : document.createElement("span");
-			if (guild.icon) (icon as HTMLImageElement).src = guild.icon.getURL(undefined, 256, "high");
-			else (icon as HTMLSpanElement).textContent = guild.name.substring(0, 2).toUpperCase();
-			guildElement.append(icon);
-		} else {
-			const icon = document.createElement("svg");
-			icon.classList.add("icon");
-			icon.innerHTML = await getIcon("question-circle");
-			guildElement.append(icon);
-		}
+		const createGuildElement = document.querySelector("button#create-guild")!;
+		createGuildElement.innerHTML = await getIcon("plus-circle");
+		const createGuildLabel = document.createElement("label");
+		createGuildLabel.textContent = "Create Guild";
+		createGuildElement.append(createGuildLabel);
 
-		const label = document.createElement("label");
-		label.textContent = guild ? guild.name : "Guild is unreachable";
-		guildElement.append(label);
+		const exploreGuildsElement = document.querySelector("button#explore-guilds")!;
+		exploreGuildsElement.innerHTML = await getIcon("compass");
+		const exploreGuildsLabel = document.createElement("label");
+		exploreGuildsLabel.textContent = "Explore Guilds";
+		exploreGuildsElement.append(exploreGuildsLabel);
 
-		return guildElement;
+		sidebar.classList.remove("loading");
 	};
-
-	const guildContainer = document.querySelector("section.guilds")!;
-	guildContainer.innerHTML = "";
-	for (const folder of discordClient.settings!.guild_folders) {
-		if (folder.id) {
-			for (const guildId of folder.guild_ids) guildContainer.append(await createGuildElement(guildId));
-		} else guildContainer.append(await createGuildElement(folder.guild_ids[0]));
-	}
-
 
 	changeState("Sending status", "warning");
 
