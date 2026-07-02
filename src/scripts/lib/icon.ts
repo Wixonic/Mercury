8d5af43a2d14806d0c9b96d2fbdb85ff0bfacdc4
Wixonic <contact@wixonic.fr>
2026-07-02 18:48:21 +0200

    feat(Icons): Use existing Collection structure for icons to improve loading performance

--- src/scripts/lib/icon.ts
+++ src/scripts/lib/icon.ts
@@ -1,9 +1,20 @@
+import { Collection } from "/scripts/lib/utils.ts";
+
+export class IconCollection extends Collection<string> {
+	async fetch(name: string): Promise<string | undefined> {
+		try {
+			const response = await fetch(`/assets/icon/${name}.svg`);
+			if (!response.ok) return undefined;
+			return await response.text();
+		} catch (error) {
+			console.error(`Failed to fetch icon: ${name}`, error);
+			return undefined;
+		}
+	};
+};
+
+export const icons = new IconCollection();
+
 export const getIcon = async (name: string): Promise<string> => {
-	try {
-		const response = await fetch(`/assets/icon/${name}.svg`);
-		return await response.text();
-	} catch (error) {
-		console.error(`Failed to fetch icon: ${name}`, error);
-		return "";
-	}
+	return await icons.get(name) ?? "";
 };
