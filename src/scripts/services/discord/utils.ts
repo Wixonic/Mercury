1060eaad00815fa01b24574f63a56c358956e8e4
Wixonic <contact@wixonic.fr>
2026-08-09 14:49:45 +0200

    fix: Avoid crash

--- src/scripts/services/discord/utils.ts
+++ src/scripts/services/discord/utils.ts
@@ -1,6 +1,6 @@
 import { Attachment } from "/scripts/services/discord/attachment.ts";
 import { Channel } from "/scripts/services/discord/channel.ts";
-import { GuildMember } from "/scripts/services/discord/guildMember";
+import { GuildMember } from "/scripts/services/discord/guildMember.ts";
 import { Message } from "/scripts/services/discord/message.ts";
 import { Role } from "/scripts/services/discord/role.ts";
 import { Snowflake } from "/scripts/services/discord/snowflake.ts";
@@ -15,7 +15,8 @@ export interface ResolvedData {
 	users?: Record<Snowflake, User<true>>;
 };
 
-export const resolveData = (data: any): ResolvedData => {
+export const resolveData = (data?: any): ResolvedData | undefined => {
+	if (!data) return;
 	if (data.attachments) for (const [id, attachment] of Object.entries(data.attachments)) data.attachments[id] = new Attachment(attachment);
 	if (data.channels) for (const [id, channel] of Object.entries(data.channels)) data.channels[id] = new Channel(channel);
 	if (data.members) for (const [id, member] of Object.entries(data.members)) data.members[id] = new GuildMember(member);
