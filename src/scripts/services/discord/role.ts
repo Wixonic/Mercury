e6c92694d1ad24729777e5b569e1b9a6bfbd10fd
Wixonic <contact@wixonic.fr>
2026-08-09 14:50:02 +0200

    feat: Improve roles

--- src/scripts/services/discord/role.ts
+++ src/scripts/services/discord/role.ts
@@ -1,16 +1,23 @@
-// import { Permission } from "/scripts/services/discord/permission.ts";
-
 import { Collection } from "/scripts/lib/utils.ts";
+import { CDNElement } from "/scripts/services/discord/cdn.ts";
 
 export class Role {
 	id: string;
 	name: string;
 	color: number;
+	icon?: CDNElement;
+	unicode_emoji?: string;
+	permissions?: string;
+	position?: number;
 
 	constructor(data: any) {
 		this.id = data.id;
 		this.name = data.name;
-		this.color = data.color;
+		this.color = data.color ?? 0;
+		if (data.icon) this.icon = new CDNElement(`/role-icons/${data.id}`, data.icon);
+		if (data.unicode_emoji) this.unicode_emoji = data.unicode_emoji;
+		if (data.permissions) this.permissions = data.permissions;
+		if (data.position !== undefined) this.position = data.position;
 	};
 };
 
@@ -18,6 +25,6 @@ export class RoleCollection extends Collection<Role> {
 	constructor(roles?: any[]) {
 		super();
 
-		if (roles) for (const data of roles) this.set(data.id, new Role(data));
+		if (roles) for (const data of roles) this.set(data.id, data instanceof Role ? data : new Role(data));
 	};
 };
